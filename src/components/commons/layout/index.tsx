f84e0dc5d9ee9c06a7ce2409c464d5d98594199e
zeon8080 <jec0228@naver.com>
2023-05-02 19:11:23 +0900

    [Design]_layout_form

---
+++ src/components/commons/layout/index.tsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+import LayoutHeader from "./header/layoutHeader";
+
+interface ILayoutPros {
+  children: JSX.Element;
+}
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const LayoutBody = styled.div``;
+
+export default function Layout(props: ILayoutPros): JSX.Element {
+  return (
+    <Container>
+      <LayoutHeader />
+      <LayoutBody>{props.children}</LayoutBody>
+    </Container>
+  );
+}
