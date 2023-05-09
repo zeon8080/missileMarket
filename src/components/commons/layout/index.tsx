afbf68f3381ed2e35cc2afbd5b37e661da7a56bb
zeon8080 <jec0228@naver.com>
2023-05-09 17:22:11 +0900

    [Feat]_recentItem

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
 import LayoutHeader from "./header/layoutHeader";
+import { useRouter } from "next/router";
+import LayoutRecent from "./recent";
 
 interface ILayoutPros {
   children: JSX.Element;
@@ -14,9 +16,13 @@ const LayoutBody = styled.div`
 `;
 
 export default function Layout(props: ILayoutPros): JSX.Element {
+  const router = useRouter();
+  const HIDDEN_RECENT = ["/join", "/login"];
+  const isHiddenRecent = HIDDEN_RECENT.includes(router.asPath);
   return (
     <Container>
       <LayoutHeader />
+      {!isHiddenRecent && <LayoutRecent />}
       <LayoutBody>{props.children}</LayoutBody>
     </Container>
   );
---
+++ src/components/commons/layout/recent/index.tsx
@@ -0,0 +1,75 @@
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const ImgBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+  border: 1px solid #dddddd;
+  width: 60px;
+  height: 60px;
+
+  > img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    /* cursor: pointer; */
+  }
+`;
+
+const Box = styled.div`
+  position: fixed;
+  right: 90px;
+  top: 110px;
+  width: 90px;
+  background-color: white;
+  border: 1px solid #dddddd;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  color: #888888;
+  margin: 10px 0;
+`;
+
+export default function LayoutRecent() {
+  const router = useRouter();
+  const [todayList, setTodayList] = useState<any>();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const todayFunc = () => {
+        let recent = JSON.parse(sessionStorage.getItem("todays") || "null");
+        if (recent !== null) setTodayList(recent.slice(0, 3));
+      };
+      todayFunc();
+    }
+  }, []);
+
+  return (
+    <>
+      <Box>
+        <Title>최근본상품</Title>
+        {todayList?.map((el: { images: any[] }) => (
+          <ImgBox>
+            <img
+              src={
+                el.images[0]
+                  ? `https://storage.googleapis.com/${el.images[0]}`
+                  : "/empty.png"
+              }
+            />
+          </ImgBox>
+        ))}
+      </Box>
+    </>
+  );
+}
